026f783d7a37192bc5a4b60f7482abaeb778fb1a
Tomás López <tomas-manuel.lopez@necomplus.com>
2020-11-26 11:33:30 +0100

    Add project files.

---
+++ Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using BlazorApp1.Shared;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUploadImage.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(IFormFile file, [FromServices]IWebHostEnvironment _hostingEnvironment)
+        {
+            var path = Path.Combine(_hostingEnvironment.WebRootPath, "Images", file.FileName);
+            using var stream = System.IO.File.Create(path);
+            await file.CopyToAsync(stream);
+            return Ok();
+        }
+    }
+}
